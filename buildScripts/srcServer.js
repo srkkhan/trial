5945fbeb5b2e9e7bec28baefab71be2bf1450a09
srkkhan <srkkhan56@yahoo.com>
2019-07-29 10:20:53 +0530

    end of morning of 29th july

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,9 +1,25 @@
+/*
+ES5 code
 var express=require("express");
 var path=require("path");
 var open=require("open");
+*/
 
-var port=3000;
-var app=express();
+//ES6 code
+import express from "express";
+import path from "path";
+import open from "open";
+import webpack from "webpack";
+import config from '../webpack.config.dev'
+
+const port=3000;
+const app=express();
+const compiler=webpack(config);
+
+app.use(require('webpack-dev-middleware')(compiler,{
+    noInfo:true,
+    publicPath:config.output.publicPath
+}));
 
 app.get('/',function(req,res){
     res.sendfile(path.join(__dirname,'../src/index.html'));
